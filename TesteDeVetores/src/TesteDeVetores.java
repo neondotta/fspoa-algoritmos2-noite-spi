f9e4ddccd38ddb94d0b503d0112d0d3baca6e28e
Rafael Guterres Jeffman <rgjeffman@senacrs.com.br>
2016-10-14 21:46:44 -0300

    Terminada implementacao de vetores. Modificado para utilizar "Generics".

--- TesteDeVetores/src/TesteDeVetores.java
+++ TesteDeVetores/src/TesteDeVetores.java
@@ -4,13 +4,33 @@ public class TesteDeVetores {
 	public void run() {
 		Console.println("Testes de Vetores");
 		
-		Vetor vetor = new Vetor();
+		Vetor<Integer> vetor = new Vetor<>();
 		
 		vetor.append(1);
 		vetor.append(2);
 		vetor.append(3);
-		vetor.append(4);
-		for (int i = 0; i < vetor.size(); i++)
+		vetor.append(5);
+		
+		vetor.insert(2, 20);
+	
+		for (int i = 0; i < vetor.size(); i++) {
 			Console.println(i, ": ", vetor.get(i));
+		}
+
+		System.out.println("==============");
+
+		int soma = 0;
+		for (int i = 0; i < vetor.size(); i++) {
+			soma += vetor.get(i);
+		}
+		System.out.println("Soma = " + soma);
+		
+		System.out.println("==============");
+		
+		vetor.remove(2);
+		
+		for (int i = 0; i < vetor.size(); i++) {
+			Console.println(i, ": ", vetor.get(i));
+		}
 	}
 }
--- TesteDeVetores/src/Vetor.java
+++ TesteDeVetores/src/Vetor.java
@@ -1,10 +1,10 @@
 
-public class Vetor {
+public class Vetor <T> {
 	
-	private int vet[] = new int[2];
+	private Object[] vet = new Object[2];
 	private int numElementos = 0;
 	
-	public void append(int valor) {
+	public void append(T valor) {
 		garantirTamanho();
 		vet[numElementos] = valor;
 		numElementos++;
@@ -12,7 +12,7 @@ public class Vetor {
 
 	private void garantirTamanho() {
 		if(numElementos == vet.length){
-			int newVet[] = new int[vet.length * 2];
+			Object newVet[] = new Object[vet.length * 2];
 			for(int i = 0; i < vet.length; i++){
 				newVet[i] = vet[i];
 			}
@@ -24,11 +24,36 @@ public class Vetor {
 		return numElementos;
 	}
 
-	public int get(int index) {
-		if(index < numElementos && index >= 0)
-			return vet[index];
-		
-		throw new ArrayIndexOutOfBoundsException(index);
+	@SuppressWarnings("unchecked")
+	public T get(int index) {
+		validaIndice(index);
+		return (T)vet[index];
+	}
+	
+	
+	private void validaIndice(int index) {
+		if(index < 0 && index >= numElementos) {
+			throw new ArrayIndexOutOfBoundsException(index);
+		}
+	}
+
+	public void insert(int index, T valor) {
+		garantirTamanho();
+		validaIndice(index);
+		for(int i = numElementos;i > index; i--){
+			vet[i] = vet[i - 1];
+		}
+		vet[index] = valor;
+		numElementos++;
+	}
+
+	public void remove(int index) {
+		validaIndice(index);
+		for (int i = index; i < numElementos-1; i++)
+		{
+			vet[i] = vet[i + 1];
+		}
+		numElementos--;
 	}
 	
 }
